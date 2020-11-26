1823f3e371ea47b6f774b9dcf0d89a93e34f6259
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-26 19:30:56 +0000

    initial

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,8 +2,8 @@
 /**
  * binary_tree_preorder - Check if node is a root
  *
- * @node: Pointer to the node
- * Return: 1 or 0
+ * @tree: Pointer to the tree
+ * @func: Pointer to the function
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
